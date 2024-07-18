909d14fd702acb98188f356d1183fc507bea6872
zoe-kch <138423129+zoe-kch@users.noreply.github.com>
2024-07-18 13:27:34 -0600

    add to database + remove functions

--- user_profile.py
+++ user_profile.py
@@ -1,5 +1,12 @@
-"""class Profile:
-    def __init__ ():
+class Profile:
+    def __init__(self, name, descriptors):
+        database[name] = database.get(name,[]) + descriptors # adds to database, whether or not name is stored
+        
+    def __repr__(self):
+        return name
+        
+    def __str__(self):
+        return name,descriptors
 
-    def __call__():
-"""
+    def remove(self, name):
+        del database[name]

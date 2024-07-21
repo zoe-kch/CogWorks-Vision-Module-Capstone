99ab48a345d399f5d170302441895f00628221a9
edwardiafos <143757414+edwardiafos@users.noreply.github.com>
2024-07-21 11:05:46 -0500

    Dialog prompt and live camera feature
    Add a dialog prompt at the start of the app to give the user the opportunity to add faces to the database before running the recognizing live camera feature.

--- database.py
+++ database.py
@@ -1,25 +1,45 @@
-### This program will create and upkeep the database for this project. ###
+### This program will create the database for this project. ###
+### It's only purpose is to create the pickle file `database.pkl`; if that already exists, don't run this program-- just update in main.py as needed ###
+
 from pathlib import Path
 import os
-import cv2
 import numpy as np
-from user_profile import Profile
+import pickle
+from user_profile import *
+from main import *
 
 database = {}
-
+image_path = Path("imgs/")
 
 def initialize_database():
-    image_path = Path("imgs/")
+    global database
+    global image_path
 
-    for image_path in os.listdir(image_path):
+    for image_dir in os.listdir(image_path):
+        profile = None
+        for image in os.listdir(f"{image_path}//{image_dir}"):
+            full_img_path = Path(image_path / image_dir / image)
+            
+            print(full_img_path)
+            img_rgb = image_to_rgb(full_img_path)
 
-        img = cv2.imread("imgs/" + image_path)
-        img = np.asarray(img)
-        descriptor_vector = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        print(type(descriptor_vector))
+            boxes = np.array(detect_faces(img_rgb))
+            print(f"The boxes of the image: {boxes}")
 
-        profile = Profile(image_path, descriptor_vector)
+            if boxes.any(): #* check if boxes are empty or not
+                print(f"Image shape: {img_rgb.shape}")
+                descriptor_vector = get_descriptors(img_rgb, np.array(boxes)[np.newaxis, :][0]) #boxes[0] because there should only be one box for one face, 
+                # descriptor_vector.shape = (N, 512)
 
-        database[profile.__repr__()] = [descriptor_vector]
+                if profile == None: # check if profile is None, then create profile
+                    profile = Profile(str(image_dir), descriptors=descriptor_vector)
+                else: # add descriptor_vector when profile exist
+                    profile.add_descriptors(descriptor_vector)
 
-print(database)
+        database[profile.name] = profile
+
+    with open('database.pkl', 'wb') as f:
+        pickle.dump(database, f)
+
+
+initialize_database()

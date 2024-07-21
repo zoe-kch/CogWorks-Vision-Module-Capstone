e8cc373bae324d1f876b2367f5262512b67c92f5
edwardiafos <143757414+edwardiafos@users.noreply.github.com>
2024-07-21 11:04:35 -0500

    Dialog prompt and live camera feature
    Add a dialog prompt at the start of the app to give the user the opportunity to add faces to the database before running the recognizing live camera feature.

--- main.py
+++ main.py
@@ -21,101 +21,30 @@ from pathlib import Path
 import os
 from typing import Union, List
 
+import tkinter as tk
+from tkinter import messagebox
+
 # this will download the pretrained weights for MTCNN and resnet
 # (if they haven't already been fetched)
 # which should take just a few seconds
 model = FacenetModel()
-threshold = None
 
 # Initialize database
 image_path = Path("imgs")
+
 database = {}
+with open('database.pkl', 'rb') as f:
+    database = pickle.load(f)
 
-def find_threshold(name: str):
+threshold = None
+with open('threshold.txt', 'r') as f:
+    threshold = float(f.readline().strip())
 
-    # might have to modify indexing based on how db/profile are setup
-    
-    # basically threshold is that for this current label/profile we are checking
-    # it is the dist of the furthest description vector to the mean of this profile 
-    # plus the mean of half the dist of that furthest vector to any other given label's mean vector
-    # idk this might be a stupid way to do it and im happy to change it
-
-    """dists_to_mean1 = {}
-
-    dists_to_all_means = []    
-
-    for vector in database[profile1]:
-        dist = cos_dist(vector, profile1.mean_descriptor)
-        dists_to_mean1[dist] = vector
-    
-    furthest = max(dists_to_mean1)
-    furthest_vector = dists_to_mean1[furthest]
-    """
-
-    global database
-
-    dists_to_mean = {} # dis: vector
-    profile = database[name]
-    mean_vector = profile.mean_descriptor
-
-    for vector in profile.descriptors: # shape (N, 512)
-        dists_to_mean[cos_dist(mean_vector, vector)] = vector
-
-    # Identify the furthest distance to the mean
-    furthest = max(dists_to_mean)
-    furthest_vector = dists_to_mean[furthest]
-
-    # Calculate errors as distance between furthest descriptor and other profiles
-    errors = []
-
-    for profile in database.values():
-        dist = cos_dist(furthest_vector, profile.mean_descriptor)
-        errors.append(dist)
-
-    threshold = np.mean(np.array(errors))+ furthest
-
-    return threshold # returns a float
-
-
-def initialize_database():
-    global database
-    global image_path
-    global threshold
-    ret_descriptors, ret_rgb_img, imgs = [], [], []
-
-    for image_dir in os.listdir(image_path):
-        profile = None
-        for i, image in enumerate(os.listdir(f"{image_path}//{image_dir}")):
-            full_img_path = Path(image_path / image_dir / image)
-            
-            print(full_img_path)
-            img = image_to_rgb(full_img_path)
-            img_rgb = image_to_rgb(full_img_path)
-
-            boxes = detect_faces(img_rgb)
-            print(f"The boxes of the image: {boxes}")
-
-            if boxes: #* check if boxes are empty or not
-                descriptor_vector = get_descriptors(img_rgb, boxes)
-
-                if profile == None: # check if profile is None, then create profile
-                    profile = Profile(str(image_dir), descriptors=descriptor_vector)
-                else: # add profile when profile exist
-                    profile.add_descriptors(descriptor_vector)
-
-                database[profile.name] = profile
-                ret_descriptors.append(descriptor_vector)
-                ret_rgb_img.append(img_rgb)
-                imgs.append(img)
-
-
-        if threshold is None and boxes:
-            threshold = find_threshold(str(image_dir))
-
-    return ret_descriptors, ret_rgb_img, imgs
 
 
 def add_descriptor_vectors_to_database(descriptor_vectors: np.ndarray, names: List[str]):
+    global database
+
     """descriptors_vectors is a shape (N, 512) array and names is a length N list."""
 
     assert len(descriptor_vectors) == len(names), f"length of descriptor_vectors and names do not match | length of vectors: {len(descriptor_vectors)}, length of names: {len(names)}"
@@ -139,6 +68,7 @@ def image_to_rgb(image_to_rgb_path, max_width=500):
         image = image[..., :-1]  # png -> RGB
         
     return image.astype(np.uint8)"""
+
     from PIL import Image, ImageOps
     image = Image.open(image_to_rgb_path)
     image = ImageOps.exif_transpose(image)  # Correct the image orientation based on EXIF data
@@ -148,37 +78,22 @@ def image_to_rgb(image_to_rgb_path, max_width=500):
         new_width = max_width
         new_height = int(new_width * aspect_ratio)
         image = image.resize((new_width, new_height))
-    return np.array(image).astype(np.uint8)
+
+    image = np.array(image).astype(np.uint8)
+
+    if image.shape[-1] == 4:
+        # Image is RGBA, where A is alpha -> transparency
+        # Must make image RGB.
+        image = image[..., :-1]  # png -> RGB
+
+    return image
 
 def cos_dist(a,b):
-    out = 1 - np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b)) # changed from @ to * because a and b have different float types 
-    # assert out == cosine_similarity(a, b) # checking to see if these functions are equivalent
+    out = 1 - np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b)) # changed from @ to * because a and b have different float types
     return out
 
-# check if cos_dist == cosine_sim
-from sklearn.metrics.pairwise import cosine_similarity
-def cosine_sim(descriptors1: np.ndarray, descriptors2: np.ndarray):
-    # assert cos_dist(descriptors1, descriptors2) == cosine_similarity(descriptors1, descriptors2) # checking to see if these functions are equivalent
-    return cosine_similarity(descriptors1, descriptors2)
 
-
-def match(descriptor_vector: np.ndarray, threshold: float):
-    """all_dists = {}
-    
-    for name,d in database.items():
-        # check is np.mean(axis=0) returns average vector im so confused :3
-        all_dists[name] = cos_dist(descriptor_vector, np.mean(d,axis=0))
-        
-    # there might be a more efficient way to do this
-    min = np.min(all_dists) # min dist
-    min_name = min(all_dists, key=all_dists.get) # name pertaining to min dist
-    
-    if min < threshold:
-        Profile.add(min_name,descriptor_vector) # adds pic to profile
-        return str(min_name) # assuming min_name is a profile stored in database
-    else:
-        print('unknown')
-        Profile.add("Unknown",descriptor_vector)"""
+def match(descriptor_vector: np.ndarray, threshold: float=threshold):
     
     ### descriptor_vectors.shape should be (1, 512)
 
@@ -198,7 +113,7 @@ def match(descriptor_vector: np.ndarray, threshold: float):
     else:
         return "Unknown"
 
-def detect_faces(image, threshold=.6):
+def detect_faces(image, threshold=.7):
     """
     It takes in an image and return a list of
     boxes that are already filtered respect to the threshold
@@ -209,6 +124,8 @@ def detect_faces(image, threshold=.6):
     # just assuming that this is the threshold
     
     # these are a list of boxes filtered after the threshold
+    if boxes is None:
+        return
     filtered = [box for box, prob in zip(boxes, probabilities) if prob > threshold] #####
     
     return filtered
@@ -217,14 +134,13 @@ def get_descriptors(image, boxes):
     return model.compute_descriptors(image, boxes)
 
 
-def draw_boxes(image, boxes, names, camera=False):
+def draw_boxes(image, boxes, names, camera: bool):
     """
     It just draw boxes
     """
-    if not camera:
+    if not camera: # if not or if, we'll never know
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    else:
-        pass
+
     print(f"Image type: {type(image)}, Image shape: {image.shape}")
     for box, name in zip(boxes, names):
         start_x, start_y = int(box[0]), int(box[1])
@@ -235,64 +151,124 @@ def draw_boxes(image, boxes, names, camera=False):
 
     return image
         
+### Dead function
+"""def main(descriptors, threshold, rgb_pic, img):
+    
+    # This will be the main function for the program, where it will call all the necessary functions
+    # and also displays interface of some sort.
 
-def main(descriptors, threshold, rgb_pic, img, camera=True):
-    """
-    This will be the main function for the program, where it will call all the necessary functions
-    and also displays interface of some sort.
+    # descriptors: np.ndarray(), shape (N, 512)
+    # threshold: float, global variable for matching
+    # rgb_pic: np.ndarray: shape
 
-    descriptors: np.ndarray(), shape (N, 512)
-    threshold: float, global variable for matching
-    rgb_pic: np.ndarray: shape
 
-    """
-    if camera:
-        video_capture = cv2.VideoCapture(0)
-        #raw_capture = cv2.VideoCapture(0)
-        WIDTH, HEIGHT = 640, 360
-        video_capture.set(3, WIDTH)
-        video_capture.set(4, HEIGHT)
+    boxes = detect_faces(rgb_pic)
+    names = [match(descriptor[np.newaxis, :], threshold) for descriptor in descriptors]
+    
+    boxes_drawn = draw_boxes(img, boxes, names)
+
+    # cv2.imshow('Some really cool and interesting name for this project', boxes_drawn)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    recognize()
+"""
+
+def main():
+    video_capture = cv2.VideoCapture(0)
+    #raw_capture = cv2.VideoCapture(0)
+    WIDTH, HEIGHT = 640, 360
+    video_capture.set(3, WIDTH)
+    video_capture.set(4, HEIGHT)
         
-        while True:
-            # capture the video
-            ret, frame = video_capture.read()
+    while True:
+        # capture the video
+        ret, frame = video_capture.read()
 
-            # mirror the frame for perfect coordination
-            if ret:
-                frame = cv2.flip(frame, 1)
+        # mirror the frame for perfect coordination
+        if ret:
+            frame = cv2.flip(frame, 1)
                 
-                frame = np.array(frame).astype(np.uint8)
-                boxes = detect_faces(frame)
+            frame = np.array(frame).astype(np.uint8) # shape (640, 360, 3)
+            boxes = detect_faces(frame)
+            if boxes:
+                descriptors = get_descriptors(frame, boxes) # shape (N, 512) array
                 names = [match(descriptor[np.newaxis, :], threshold) for descriptor in descriptors]
                 
                 boxes_drawn = draw_boxes(frame, boxes, names, camera=True)
-                cv2.imshow('Some really cool and interesting name for this project', boxes_drawn)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+                cv2.imshow('This You?', boxes_drawn)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-        video_capture.release()
-        cv2.destroyAllWindows()
-        
-    else:
+    video_capture.release()
+    cv2.destroyAllWindows()
 
-        boxes = detect_faces(rgb_pic)
-        names = [match(descriptor[np.newaxis, :], threshold) for descriptor in descriptors]
-        
-        boxes_drawn = draw_boxes(img, boxes, names)
+def recognize():
+    global threshold
 
-        cv2.imshow('Some really cool and interesting name for this project', boxes_drawn)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-    
-    
+    print("SMILE!!")
+    img_array = take_picture() # note: add to read me that the user will have to configure their Camera package before using this application
+    print("Took a picture!")
 
+    boxes = detect_faces(img_array)
+    if boxes: #* check if boxes are empty or not
+        print("Camera succesfully detected face")
+        descriptor_vector = get_descriptors(img_array, boxes) # (N, 512) array
 
+    print(f"descriptor vector of camera person(s): {descriptor_vector[:, :10], descriptor_vector.shape}")
+    names = [match(descriptor[np.newaxis, :], threshold) for descriptor in descriptor_vector]
+    print(f"Name(s) of person(s) in camera: {names}")
 
+    box_drawn = draw_boxes(img_array, boxes, names)
+
+    cv2.imshow('This You?', box_drawn)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    vars = initialize_database()
-    descriptors, rgb_pics, imgs = vars
-    print(f"Returns of initialize_database exist? : {descriptors and rgb_pics}")
-    print(f"Length of descriptors passed to main function: {len(descriptors)} | length of images: {len(rgb_pics)}")
-    for des, rgbpic, img in zip(descriptors, rgb_pics, imgs):
-        main(des, threshold, rgbpic, img)
+    # vars = initialize_database()
+    # descriptors, rgb_pics, imgs = vars
+    # print(f"Returns of initialize_database exist? : {descriptors and rgb_pics}")
+    # print(f"Length of descriptors passed to main function: {len(descriptors)} | length of images: {len(rgb_pics)}")
+    # for des, rgbpic, img in zip(descriptors, rgb_pics, imgs):
+    #    main(des, threshold, rgbpic, img)
+
+    root = tk.Tk()
+    root.withdraw()
+    add_to_database = messagebox.askyesno("Add photos to Database?", "Do you want to take some pictures to add to our database?")
+
+    if add_to_database:
+        name = []
+        def get_name():
+            global name
+            name = [entry.get()]
+            root.destroy()
+
+        root.deiconify()
+        root.geometry("500x100+700+300")
+        label = tk.Label(root, text="What's your name?")
+        label.pack()
+        entry = tk.Entry(root)
+        entry.pack()
+
+        button = tk.Button(root, text="Enter", command=get_name)
+        button.pack()
+        root.mainloop()
+
+        root = tk.Tk()
+        messagebox.showinfo("Going to take database photos", "We're going to take 5 pictures of you to add to our database. Make sure to smile!!")
+        root.withdraw()
+
+        photos = [take_picture() for i in range(5)]
+        boxes = [np.array(detect_faces(photo)) for photo in photos]
+        descriptor_vectors = [get_descriptors(photo, box) for photo, box in zip(photos, boxes) if box.any()]
+        descriptor_vectors = np.array([dv for des_vec in descriptor_vectors for dv in des_vec]) # get shape (N, 512) array then index each shape (512,) item in the array
+
+        name = name * len(descriptor_vectors)
+
+        add_descriptor_vectors_to_database(descriptor_vectors, name)
+
+    root.deiconify()
+    messagebox.showinfo("Live Camera", "Enabling our live camera. To quit the program, press 'q'. Say hi to yourself!")
+    root.withdraw()
+    main()
